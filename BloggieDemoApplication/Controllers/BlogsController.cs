d5d1f9d9b2a1b95dd2b3fc0657ac160b96fe33df
Sandiso2080 <91184967+Sandiso2080@users.noreply.github.com>
2024-07-10 13:45:37 +0200

    button Links
    button UrL

---
+++ BloggieDemoApplication/Controllers/BlogsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BloggieDemoApplication.Controllers
+{
+    public class BlogsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(string urlHandle)
+        {
+            return View();
+        }
+    }
+}
